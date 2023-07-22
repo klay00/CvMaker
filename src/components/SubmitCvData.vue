69ffb9b6ff752193514f75ae8fcca56561a094a3
klay00 <aali74432@gmail.com>
2023-07-22 22:00:08 +0300

    create submit cv component

---
+++ src/components/SubmitCvData.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="left-info">
+        <h1>Submit Cv</h1>
+
+        <div class="buttnes ">
+            <button class=" btn-back" v-if="counter > 0" @click="$emit('back')"> Back</button>
+            <button class="btn" v-if="counter < 5" @click="$emit('next')">Next</button>
+        </div>
+        
+        </div>
+</template>
+
+<script>
+export default {
+    props:['counter']
+}
+</script>
+
+<style>
+
+</style>
