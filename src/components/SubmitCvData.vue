9cc54d8adb0c6fb5b5ba82fa9c081e1758319917
klay00 <aali74432@gmail.com>
2023-07-24 19:15:08 +0300

    add btn to print or download the cv

--- src/components/SubmitCvData.vue
+++ src/components/SubmitCvData.vue
@@ -1,7 +1,9 @@
 <template>
    <div class="left-info">
         <h1>Submit Cv</h1>
-
+           <div class="print-btn">
+            <button class="btn" @click="handelPrint">Print Or Download</button>
+           </div>
         <div class="buttnes ">
             <button class=" btn-back" v-if="counter > 0" @click="$emit('back')"> Back</button>
             <button class="btn" v-if="counter < 5" @click="$emit('next')">Next</button>
@@ -12,10 +14,19 @@
 
 <script>
 export default {
-    props:['counter']
+    props:['counter'],
+    methods:{
+        handelPrint(){
+            window.print();
+        }
+    }
 }
 </script>
 
 <style>
-
+.print-btn{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
 </style>
